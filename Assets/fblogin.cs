664cf18487e1e80bf930e33aaa4b783d1be93b65
heart <narongrit@3dsinteractive.com>
2016-12-04 02:58:33 +0700

    no message

---
+++ Assets/fblogin.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Facebook.Unity;
+using UnityEngine.UI;
+using Facebook.MiniJSON;
+using UnityEngine.SceneManagement;
+
+public class fblogin : MonoBehaviour {
+	void Awake(){
+		FB.Init(SetInit, onHideUnity);
+	}
+	private void SetInit(){
+		if (FB.IsInitialized) {
+			FB.ActivateApp ();
+		} else {
+			Debug.Log ("Failed to Initialized");
+		}
+	}
+	private void onHideUnity(bool isGameShown){
+		if (!isGameShown)
+		{
+			Time.timeScale = 0;
+		}
+		else
+		{
+			Time.timeScale = 1;
+		}
+
+	}
+	public void FBlogin(){
+		var perms = new List<string>() { "public_profile", "email", "user_friends" };
+		FB.LogInWithReadPermissions(perms, AuthCallback);
+
+	}
+	public void AuthCallback(ILoginResult result){
+		if (FB.IsLoggedIn){
+			LoadStage ();
+			var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+			Debug.Log(aToken.UserId);
+			foreach (string perm in aToken.Permissions){
+				Debug.Log(perm);
+			}
+		}
+		else{
+			Debug.Log("User cancelled login");
+		}
+
+	}
+	public void LoadStage()  {
+		
+		SceneManager.LoadScene ("playgame");
+		//Application.LoadLevel ("playgame");
+	}
+	public void FBlogout(){
+		FB.LogOut ();
+		SceneManager.LoadScene ("main");
+		//Application.LoadLevel ("main");
+	}
+}
---
+++ Assets/profile.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Facebook.MiniJSON;
+using Facebook.Unity;
+
+public class profile : MonoBehaviour {
+
+	public GameObject fbname;
+	public GameObject fbava;
+
+
+
+	public profile(){
+		
+	}
+
+	void Start(){
+		
+		fbname = GameObject.Find ("fbname");
+		fbava = GameObject.Find("fbava");
+
+		loadUserName();
+		loadProfilePicture();
+	}
+
+	public void loadProfilePicture(){
+		FB.API( "me/picture?width=128&height=128" , HttpMethod.GET, DealWithProfilePicture);
+	}
+	public void loadUserName(){
+		FB.API ("/me?filed=id,first_name", HttpMethod.GET, DealWithUsername);
+	}
+
+	private void DealWithProfilePicture(IGraphResult result){
+		if (result.Error != null) 
+		{
+			Debug.Log ("problem with getting profile");
+		}
+		Image UserAvatar = fbava.GetComponent<Image> ();
+		UserAvatar.sprite = Sprite.Create (result.Texture, new Rect (0, 0, 128, 128), new Vector2 (0, 0));
+		return;
+	}
+	private void DealWithUsername(IGraphResult result){
+		if(result.Error != null)
+		{
+			Debug.Log ("problem with getting username");
+			return;
+		}
+
+		Debug.Log( result.RawResult );
+
+		Dictionary<string, object> profile =  Json.Deserialize(result.RawResult) as Dictionary<string,object>;
+
+		string name = profile["name"] as string;
+
+
+		Text Username = fbname.GetComponent<Text> ();
+		Username.text = "Hello, " + name;
+	}
+
+}
